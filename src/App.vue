73492233b2ae92448d39074be39d1aee11b238f6
olga_demina <olga.demina1410@gmail.com>
2023-11-23 13:09:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script></script>
+
+<template>
+  <h1>Hello</h1>
+  <section class="main-section">
+    <h2>
+      Section title
+      <span class="highlight">Evidenzia</span>
+    </h2>
+    <p class="subtitle">Sottotitolo</p>
+  </section>
+
+  <section class="main-section dark">
+    <h2>Altro titolo</h2>
+  </section>
+
+  <button class="button">
+    My button
+    <span>icon</span>
+  </button>
+
+  <ul class="my-list">
+    <li>item</li>
+    <li>item</li>
+    <li>item</li>
+    <li>item</li>
+    <li>item</li>
+  </ul>
+</template>
+
+<style lang="scss">
+@use "./style/general.scss";
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+    <div class="title-wrapper">
+        <h2>Hello title</h2>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.title-wrapper {
+    h2 {
+        
+    }
+}
+</style>
